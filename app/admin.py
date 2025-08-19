598fe1e9b893707425e99ef7b02353c74c181460
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-19 23:02:22 +0530

    refactor admin.py to improve model registration formatting

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Class, TeacherProfile, Division, StudentProfile, Subject
 # Register your models here.
+
+admin.site.register(Class)
+admin.site.register(TeacherProfile)
+admin.site.register(Division)
+admin.site.register(StudentProfile)
+admin.site.register(Subject)
