f6b845ea7b7699011e0fa5dc1c0f41e3c98d0927
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-19 23:18:29 +0530

    refactor urls.py to update login path and maintain consistency in route definitions

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signin', views.signin, name='signin'),
-    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    # path('register', views.register, name='register'),
+    path('contact', views.contact, name='contact'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('signout', views.signout, name='signout'),
+    path('reset_password/', views.reset_password, name='reset_password'),
 
 ]
