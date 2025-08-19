a6085b9a49f65ffeadaaede6ee2bb6c70f4a7739
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-19 11:04:48 +0530

    refactor urls.py for improved code

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,11 @@
-from django.contrib import admin
 from django.urls import path
-import include
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('signin',views.signin,name='signin'),
-    path('register',views.register,name='register'),
-    path('dashboard',views.dashboard,name='dashboard'),
-    path('signout',views.signout,name='signout'),
+    path('', views.index, name='index'),
+    path('signin', views.signin, name='signin'),
+    path('register', views.register, name='register'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('signout', views.signout, name='signout'),
 
 ]
